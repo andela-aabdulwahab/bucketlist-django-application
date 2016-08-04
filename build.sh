5f05f34fd453c29dcab602cfdb121aa00408427b
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-04 10:40:46 +0100

    [feature] add static url

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-python manage.py collectstatic --noinput --settings=bucketlist_application.settings
 python manage.py makemigrations
 python manage.py migrate
 gunicorn bucketlist_application.wsgi --log-file=-
