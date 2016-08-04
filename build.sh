0e1c038cc14ba131ad2858c97b0b883ca1b76d9b
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-04 10:09:07 +0100

    [feature] modify static_root path

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+python manage.py collectstatic --noinput --settings=bucketlist_application.settings
 python manage.py makemigrations
 python manage.py migrate
 gunicorn bucketlist_application.wsgi --log-file=-
